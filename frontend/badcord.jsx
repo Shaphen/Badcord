bb0c4c472e0aa1070bda63696e69c8373f39a1aa
Shaphen <swpburn@gmail.com>
2020-04-27 17:03:16 -0700

    add api_users_controller and can connect to splash page

--- frontend/badcord.jsx
+++ frontend/badcord.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 document.addEventListener("DOMContentLoaded", () => {
-  root = document.getElementById('root');
+  const root = document.getElementById('root');
   ReactDOM.render(<h1>Badcord</h1>, root)
 });
