7f6183ec1c60e36aabd6d858164feeebabe44c4b
Shaphen <swpburn@gmail.com>
2020-04-28 16:48:27 -0700

    Add small styling to splash page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,9 +6,10 @@ import SignupContainer from './session_form/signup_container'
 import { AuthRoute } from '../util/route_util';
 
 const App = () => (
-  <div>
-    <header>
-      <h1>Badcord Component</h1>
+  <div className="splash">
+    <header classname="splash-header">
+      <h3>The same way to chat with your communities and friends</h3>
+      <p>Badcord is the second easiest way to communicate over only text (because the others are too much work), whether you’re part of a mob, a nightly robber group, a worldwide assassins guild, or just a handful of friends that want to atack things.</p>
     </header>
     <Switch>
       <AuthRoute path="/login" component={LoginContainer} />
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,11 +5,11 @@ export default ({ currentUser, logout }) => {
   
   const display = currentUser ? (
     <div>
-      <h3>Logged in as: { currentUser.username }</h3>
+      <p className="greeting-login-state">Logged in as: { currentUser.username }</p>
       <button onClick={ logout }>Log Out</button>
     </div>
   ) : (
-    <div>
+    <div className="signup-login">
         <Link to="/signup">Sign Up</Link>
         <Link to="/login">Log In</Link>
     </div>
