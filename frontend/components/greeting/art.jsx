19f8bf7be14628d5180331293664901161148d7d
Shaphen <swpburn@gmail.com>
2020-04-29 16:23:41 -0700

    Updates to splash page content

--- frontend/components/greeting/art.jsx
+++ frontend/components/greeting/art.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 
 const Art = () => {
   return (
-    <div className="art">
+    <div className="art-box">
       <img className="android" src={window.android} />
+      <img className="iphone" src={window.iphone} />
+      <img className="controller" src={window.controller} />
+      <img className="desktop" src={window.desktop} />
+      <img className="laptop" src={window.laptop} />
+      <img className="potion" src={window.potion} />
+      <img className="disc" src={window.disc} />
+      <img className="headphones" src={window.headphones} />
     </div>
   )
 }
