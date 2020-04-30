3999a1b542288c6685e5c0b17e106d04ce7ae597
Shaphen <swpburn@gmail.com>
2020-04-30 15:02:48 -0700

    Adjust routes and add index to servers controller

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,13 +1,14 @@
 class Api::ServersController < ApplicationController
   def index
-
+    @servers = Server.all
+    render :index
   end
 
   def create
-
+    
   end
 
   def destroy
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'servers/index'
-  get 'servers/create'
-  get 'servers/edit'
-  get 'servers/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create]
+    resources :users, only: [:create] do
+      resources :servers, only: [:create, :destroy]
+    end
+    resources :servers, only: [:index]
+
     resource :session, only: [:create, :destroy]
   end
   
