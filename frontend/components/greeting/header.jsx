464698c6f472d7eb6f29821dccf00ffcb7618a8e
Shaphen <swpburn@gmail.com>
2020-04-29 21:11:20 -0700

    Complete demo login button on splash page

--- frontend/components/greeting/header.jsx
+++ frontend/components/greeting/header.jsx
@@ -4,42 +4,51 @@ import { AiFillGithub }from 'react-icons/ai';
 import { AiOutlineLinkedin } from 'react-icons/ai'
 
 
-export default ({ currentUser, logout, login }) => {
-
-  const demoLogin = () => {
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.demoLogin = this.demoLogin.bind(this);
+  }
+  
+  demoLogin(e) {
+    e.preventDefault();
     const user = {
       username: "shaphen",
       password: "password"
     }
-    login(user)
+    this.props.login(user)
   }
   
-  const display = currentUser ? (
-    <div className="greeting-container">
-      <p className="greeting-login-state">Logged in as: {currentUser.username} (temp. confirmation)</p>
-      <button className="logout-button" onClick={logout}>LOGOUT</button>
-    </div>
-  ) : (
-    <div className="header-box">
-      <div className="header-logo-side">
-        <img className="logo-white" src={window.mono_white_logo} />
+  render() {
+    const display = this.props.currentUser ? (
+      <div className="greeting-container">
+        <p className="greeting-login-state">Logged in as: {this.props.currentUser.username} (temp. confirmation)</p>
+        <button className="logout-button" onClick={this.props.logout}>LOGOUT</button>
       </div>
-      <div className="login-box-side">
-        <a className="linkedin-icon" href="https://www.linkedin.com/in/shaphen-pangburn-a27817151/" target="_blank">
-            <AiOutlineLinkedin fill="white" size={20} />
-        </a>
-        <a className="github-icon" href="https://github.com/Shaphen/Badcord/wiki" target="_blank">
-          <AiFillGithub fill="white" size={20} />
-        </a>
-        {/* <button className="demo-login" onClick={demoLogin()}>Demo Login</button> */}
-        <Link to="/login" className="header-login-button">Log In</Link>
+    ) : (
+      <div className="header-box">
+        <div className="header-logo-side">
+          <img className="logo-white" src={window.mono_white_logo} />
+        </div>
+        <div className="login-box-side">
+          <a className="linkedin-icon" href="https://www.linkedin.com/in/shaphen-pangburn-a27817151/" target="_blank">
+              <AiOutlineLinkedin fill="white" size={20} />
+          </a>
+          <a className="github-icon" href="https://github.com/Shaphen/Badcord/wiki" target="_blank">
+            <AiFillGithub fill="white" size={20} />
+          </a>
+          <button className="demo-login" onClick={(e) => this.demoLogin(e)}>Demo Login</button>
+          <Link to="/login" className="header-login-button">Log In</Link>
+        </div>
       </div>
-    </div>
-    )
+      )
+      return (
+        <div className="header">
+          {display}
+        </div>
+      )
+  }
 
-  return (
-    <div className="header">
-      {display}
-    </div>
-  )
 }
+
+export default Header;
