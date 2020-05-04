d98de1e34d31c9281d52ea22e52b7ce8857b1a2f
Shaphen <swpburn@gmail.com>
2020-05-04 12:14:22 -0700

    Fix overflow hidden bug with splash page post-relocation HEaderContainer into Art

--- frontend/components/greeting/art.jsx
+++ frontend/components/greeting/art.jsx
@@ -4,36 +4,38 @@ import HeaderContainer from './header_container';
 
 const Art = () => {
   return (
-    <div className="art-box">
-      <HeaderContainer />
-      <img className="android" src={window.android} />
-      <img className="iphone" src={window.iphone} />
-      <img className="controller" src={window.controller} />
-      <img className="desktop" src={window.desktop} />
-      <img className="laptop" src={window.laptop} />
-      <img className="potion" src={window.potion} />
-      <img className="disc" src={window.disc} />
-      <img className="headphones" src={window.headphones} />
-      <img className="mariobox" src={window.mariobox} />
-      <img className="gamepak floating" src={window.gamepak} />
-      <img className="bomb floating2" src={window.bomb} />
-      <img className="coin1 floating" src={window.coin} />
-      <img className="coin2 floating" src={window.coin} />
-      <img className="triangle1" src={window.triangle} />
-      <img className="triangle2" src={window.triangle} />
-      <img className="circle1" src={window.circle} />
-      <img className="circle2" src={window.circle} />
-      <img className="circle3" src={window.circle} />
-      <img className="circlefill1" src={window.circlefill} />
-      <img className="circlefill2" src={window.circlefill} />
-      <img className="circlefill3" src={window.circlefill} />
-      <img className="circlefill4" src={window.circlefill} />
-      <img className="circlefill5" src={window.circlefill} />
-      <img className="square1" src={window.square} />
-      <img className="square2" src={window.square} />
-      <img className="square3" src={window.square} />
-      <img className="x1" src={window.x} />
-      <img className="x2" src={window.x} />
+    <div>
+        <HeaderContainer />
+      <div className="art-box">
+        <img className="android" src={window.android} />
+        <img className="iphone" src={window.iphone} />
+        <img className="controller" src={window.controller} />
+        <img className="desktop" src={window.desktop} />
+        <img className="laptop" src={window.laptop} />
+        <img className="potion" src={window.potion} />
+        <img className="disc" src={window.disc} />
+        <img className="headphones" src={window.headphones} />
+        <img className="mariobox" src={window.mariobox} />
+        <img className="gamepak floating" src={window.gamepak} />
+        <img className="bomb floating2" src={window.bomb} />
+        <img className="coin1 floating" src={window.coin} />
+        <img className="coin2 floating" src={window.coin} />
+        <img className="triangle1" src={window.triangle} />
+        <img className="triangle2" src={window.triangle} />
+        <img className="circle1" src={window.circle} />
+        <img className="circle2" src={window.circle} />
+        <img className="circle3" src={window.circle} />
+        <img className="circlefill1" src={window.circlefill} />
+        <img className="circlefill2" src={window.circlefill} />
+        <img className="circlefill3" src={window.circlefill} />
+        <img className="circlefill4" src={window.circlefill} />
+        <img className="circlefill5" src={window.circlefill} />
+        <img className="square1" src={window.square} />
+        <img className="square2" src={window.square} />
+        <img className="square3" src={window.square} />
+        <img className="x1" src={window.x} />
+        <img className="x2" src={window.x} />
+      </div>
     </div>
   )
 }
