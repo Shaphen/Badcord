e21350cbd26f0deec426bbb66c82b62847a9b203
Shaphen <swpburn@gmail.com>
2020-05-05 22:17:59 -0700

    Add channel reducer to frontend

---
+++ frontend/util/channel_api_util.js
@@ -0,0 +1,26 @@
+
+
+export const fetchChannels = () => $.ajax({
+  url: '/api/channels'
+});
+
+export const fetchChannel = channelId => $.ajax({
+  url: `/api/channels/${channelId}`,
+});
+
+export const createChannel = channel => $.ajax({
+  url: '/api/channels',
+  method: 'POST',
+  data: { channel }
+});
+
+export const updateChannel = channel => $.ajax({
+  url: `/api/channels/${channel.id}`,
+  method: 'PATCH',
+  data: { channel }
+});
+
+export const deleteChannel = channelId => $.ajax({
+  url: `/api/channels/${channelId}`,
+  method: 'DELETE'
+});
