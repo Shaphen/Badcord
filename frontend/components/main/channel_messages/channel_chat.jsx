4ea31fc7aee82e7f239b0a192ffed866d6c0b035
Shaphen <swpburn@gmail.com>
2020-05-07 12:09:02 -0700

    Complete draft styling message box

--- frontend/components/main/channel_messages/channel_chat.jsx
+++ frontend/components/main/channel_messages/channel_chat.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 class ChannelChat extends React.Component {
   constructor(props) {
@@ -9,13 +10,22 @@ class ChannelChat extends React.Component {
     return (
       <div id="channel-chat-container">
         <div id="channel-chat-header">
-
+          <p id="channel-chat-header-hash">#</p>
+          <p id="channel-chat-header-name">Channel Name</p>
+          <p id="members-list-icon"><BsFillPersonLinesFill size={ 22 } /></p>
         </div>
         <div id="channel-chat-box">
-          <p>Welcome to the chat channel!</p>
-        </div>
-        <div id="channel-chat-new-message-box">
-
+          <div id="chat-box">
+            <div id="chat-box-welcome">
+              <p id="welcome-text-main">Welcome to the chat channel!</p>
+              <p id="welcome-text-sub">This is the state of the channel</p>
+              <label id="welcome-text-edit-channel">Edit Channel (placeholder)</label>
+            </div>
+            <div>
+              {/* -real chat goes here- */}
+            </div>
+          </div>
+            <input type="text" id="channel-chat-new-message-box" placeholder="Message channel" />
         </div>
       </div>
     )
