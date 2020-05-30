d7b50b07f9298c709a1b71edc1c0314aeb7b4df4
Shaphen <swpburn@gmail.com>
2020-05-29 22:36:13 -0700

    Reposition error toast to top center

--- frontend/components/session_form/session.jsx
+++ frontend/components/session_form/session.jsx
@@ -72,31 +72,64 @@ class Session extends React.Component {
     const name = this.props.formType === 'Welcome Back!' ? 'Register' : 'Already have an account?'
     return (
       <div className="login-signup-background">
+        <ToastContainer id="toast" position="top-center" />
         <img className="login-signup-bg-img" src={window.login_signup_bg} />
-          <img className="login-signup-bg-logo" src={window.mono_logo} />
+        <img className="login-signup-bg-logo" src={window.mono_logo} />
         <div id="session">
-          <Link to="/" className="login-signup-go-back">X</Link>
+          <Link to="/" className="login-signup-go-back">
+            X
+          </Link>
           <div id="form-box">
-            <h1 className="form-type">{ this.props.formType }</h1>
-            <form className="login-signup-form" onSubmit={ this.handleSubmit }>
+            <h1 className="form-type">{this.props.formType}</h1>
+            <form className="login-signup-form" onSubmit={this.handleSubmit}>
               <label className="username">
                 <p>USERNAME</p>
-                <input type="text" value={this.state.username} onChange={this.handleChange('username')} />
+                <input
+                  type="text"
+                  value={this.state.username}
+                  onChange={this.handleChange("username")}
+                />
               </label>
-              {
-                this.props.formType === 'Create an account' ? <label id="email">EMAIL</label>: ''
-              }
-              {
-                this.props.formType === 'Create an account' ? <input type="email" className="email-box" value={this.state.email} onChange={this.handleChange('email')}/> : ''
-              }
+              {this.props.formType === "Create an account" ? (
+                <label id="email">EMAIL</label>
+              ) : (
+                ""
+              )}
+              {this.props.formType === "Create an account" ? (
+                <input
+                  type="email"
+                  className="email-box"
+                  value={this.state.email}
+                  onChange={this.handleChange("email")}
+                />
+              ) : (
+                ""
+              )}
               <label className="password">
                 <p>PASSWORD</p>
-                <input type="password" value={this.state.password} onChange={this.handleChange('password')} />
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.handleChange("password")}
+                />
               </label>
-              <button className="session-submit-button" value={this.props.formType}>Submit</button>
-              <button onClick={(e) => this.demoLogin(e)} className="demo-login">Demo Login</button>
-              {this.props.formType === 'Welcome Back!' ? <label id="other-form-text">Need an account?</label> : "" }
-              <Link to={address} className="other-form">{name}</Link>
+              <button
+                className="session-submit-button"
+                value={this.props.formType}
+              >
+                Submit
+              </button>
+              <button onClick={(e) => this.demoLogin(e)} className="demo-login">
+                Demo Login
+              </button>
+              {this.props.formType === "Welcome Back!" ? (
+                <label id="other-form-text">Need an account?</label>
+              ) : (
+                ""
+              )}
+              <Link to={address} className="other-form">
+                {name}
+              </Link>
             </form>
           </div>
           <div id="qr-box">
@@ -107,13 +140,9 @@ class Session extends React.Component {
           <div id="errors-box">
             {/* { this.props.errors.length ? this.renderErrors() : null } */}
           </div>
-          <ToastContainer
-            id="toast"
-            position="top-center"
-          />
         </div>
       </div>
-    )
+    );
   }
 }
 
