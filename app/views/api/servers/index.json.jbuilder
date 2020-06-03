4113bee234357fd99cdac2f5b4c7238534758d26
Shaphen <swpburn@gmail.com>
2020-06-02 22:12:52 -0700

    route to general channel when clicking server in index

--- app/views/api/servers/index.json.jbuilder
+++ app/views/api/servers/index.json.jbuilder
@@ -1,7 +1,7 @@
 
 @servers.each do |server|
   json.set! server.id do
-    json.extract! server, :id, :name, :member_ids, :owner_id, :invite_code
+    json.extract! server, :id, :name, :member_ids, :owner_id, :invite_code, :channel_ids
     if server.photo.attached?
       json.photoUrl url_for(server.photo)
     end
--- frontend/components/main/servers/server_display/server_index-item.jsx
+++ frontend/components/main/servers/server_display/server_index-item.jsx
@@ -10,10 +10,9 @@ const ServerIndexItem = (props) => {
     })
   }
 
-  // debugger
   return (
     <div id="server-box">
-      <Link to={`/channels/${props.server.id}`} className="server-link">
+      <Link to={`/channels/${props.server.id}/${props.server.channel_ids[0]}`} className="server-link">
         <p id="server-button2">{props.server.photoUrl ? <img id="server-img" src={props.server.photoUrl} alt=""/> : name }</p>
       </Link>
       <p id="server-name-display">{props.server.name}</p>
