4dc807910d7288c8a6b7d2485d6f45905e09463c
Shaphen <swpburn@gmail.com>
2020-06-03 23:00:30 -0700

    Temp disable default avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :email, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
-  after_initialize :ensure_session_token, :ensure_default_photo
+  after_initialize :ensure_session_token #, :ensure_default_photo
 
   attr_reader :password
 
