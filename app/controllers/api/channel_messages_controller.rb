f494160ebfc4c17990d8167e045b187492ed5f2a
Shaphen <swpburn@gmail.com>
2020-06-15 21:42:00 -0700

    Add successful deletion cycle to chat messages

--- app/controllers/api/channel_messages_controller.rb
+++ app/controllers/api/channel_messages_controller.rb
@@ -22,15 +22,16 @@ class Api::ChannelMessagesController < ApplicationController
   #   end
   # end
 
-  # def destroy
-  #   @message = ChannelMessage.find_by(id: params[:id])
-  #   if @message
-  #     @message.destroy
-  #     render json: ["It worked!"], status: 200
-  #   else
-  #     render json: ["Could not find or delete message. Big sad"], status: 422
-  #   end
-  # end
+  def destroy
+    @message = ChannelMessage.find(params[:id])
+    debugger
+    if @message
+      @message.destroy
+      render json: ["It worked!"], status: 200
+    else
+      render json: ["Could not find or delete message. Big sad"], status: 422
+    end
+  end
 
   # private
   # def channel_message_params
