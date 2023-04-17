0352072ce5c0bdce9224c96d6b6f4e50532d7a7f
Shaphen <swpburn@gmail.com>
2023-04-17 10:42:45 -0700

    Probably dont merge

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -6,11 +6,14 @@ export const signupUser = user => $.ajax({
   data: { user }
 });
 
-export const loginUser = user => $.ajax({
-  url: '/api/session',
-  method: 'POST',
-  data: { user }
-});
+export const loginUser = user =>{ 
+  let response = $.ajax({
+    url: '/api/session',
+    method: 'POST',
+    data: { user }
+  })
+  return response;
+};
 
 export const logoutUser = () => $.ajax({
   url: '/api/session',
